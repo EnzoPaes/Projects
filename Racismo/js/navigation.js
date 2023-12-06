35709850ab1180d8e0e4c4561be2caf60e62aa5a
eidicaro <icaroeid@gmail.com>
2023-12-06 11:08:31 -0300

    finalização do projeto

--- Racismo/js/navigation.js
+++ Racismo/js/navigation.js
@@ -1,20 +1,25 @@
-const sideMenu = document.querySelector("aside");
-const headerBar = document.querySelector("header");
-document.addEventListener("click", (e)=>{
-    if(!e.path.includes(sideMenu)){
-        sideMenu.classList.remove("active");
-    } 
-});
+// Seleção de elementos no DOM
+const sideMenu = document.querySelector("aside");  // Seleciona o elemento <aside> na página
+const headerBar = document.querySelector("header");  // Seleciona o elemento <header> na página
 
-document.addEventListener("scroll", (e)=>{
-    if(window.scrollY > 50 && !headerBar.classList.contains("fixed")){
-        headerBar.classList.add("fixed");
-    }
-    if(window.scrollY < 50){
-        headerBar.classList.remove("fixed");
+// Adiciona um evento de clique no documento para fechar o menu lateral se o clique estiver fora dele
+document.addEventListener("click", (e) => {
+    if (!e.path.includes(sideMenu)) {  // Verifica se o clique não está dentro do elemento do menu lateral
+        sideMenu.classList.remove("active");  // Remove a classe "active" para ocultar o menu lateral
     }
 });
 
-function menuToggle(){
-    sideMenu.classList.toggle("active");
+// Adiciona um evento de rolagem para fixar o cabeçalho quando a rolagem da página ultrapassa 50 pixels
+document.addEventListener("scroll", (e) => {
+    if (window.scrollY > 50 && !headerBar.classList.contains("fixed")) {
+        headerBar.classList.add("fixed");  // Adiciona a classe "fixed" ao cabeçalho para torná-lo fixo
+    }
+    if (window.scrollY < 50) {
+        headerBar.classList.remove("fixed");  // Remove a classe "fixed" quando a rolagem está abaixo de 50 pixels
+    }
+});
+
+// Função para alternar a visibilidade do menu lateral
+function menuToggle() {
+    sideMenu.classList.toggle("active");  // Adiciona ou remove a classe "active" para mostrar ou ocultar o menu lateral
 }
--- Racismo/js/resolution.js
+++ Racismo/js/resolution.js
@@ -1,3 +1,4 @@
+// Seleção de elementos no DOM
 const btnAdicionar = document.getElementById("btn-adicionar");
 const ajudaTotal = document.getElementById("ajuda-total");
 const ajudaMedia = document.getElementById("ajuda-media");
@@ -5,48 +6,51 @@ const txtPessoa = document.getElementById("txtpessoa");
 const txtValor = document.getElementById("txtvalor");
 const lista = document.querySelector(".list");
 
-console.log(txtPessoa);
-
+// Array para armazenar objetos de ajuda
 const AJUDAS = [];
 
+// Função para atualizar o total de ajudas
 function atualizarTotal(AJUDAS) {
-    ajudaTotal.innerHTML = "";
+    ajudaTotal.innerHTML = ""; // Limpa o conteúdo HTML do elemento com id "ajuda-total"
     var total = 0;
     for (let index = 0; index < AJUDAS.length; index++) {
-        total = total + 1;
+        total = total + 1;  // Conta o número total de ajudas
     }
 
     ajudaTotal.innerHTML = `${total}`;
     return total;
 }
 
+// Função para atualizar a média das ajudas
 function atualizarMedia(AJUDAS) {
     ajudaMedia.innerHTML = "";
     if (AJUDAS.length === 0) {
-
-        ajudaMedia.innerHTML = "0⭐";
+        ajudaMedia.innerHTML = "0⭐";  // Se não houver ajudas, exibe 0 estrelas
         return;
     }
 
     var soma = 0;
     for (let index = 0; index < AJUDAS.length; index++) {
-        soma += parseInt(AJUDAS[index].valor);
+        soma += parseInt(AJUDAS[index].valor);  // Soma os valores das ajudas
     }
 
-    var media = soma / AJUDAS.length;
-    ajudaMedia.innerHTML = `${media.toFixed(2)}⭐`;
+    var media = soma / AJUDAS.length;  // Calcula a média
+    ajudaMedia.innerHTML = `${media.toFixed(2)}⭐`;  // Exibe a média com duas casas decimais
 }
 
+// Função para remover uma ajuda com base no índice
 function removerAjuda(id) {
-    AJUDAS.splice(id, 1);
-    listarAjudas();
-    atualizarTotal(AJUDAS);
-    atualizarMedia(AJUDAS);
+    AJUDAS.splice(id, 1);  // Remove a ajuda do array
+    listarAjudas();  // Atualiza a lista de ajudas no DOM
+    atualizarTotal(AJUDAS);  // Atualiza o total
+    atualizarMedia(AJUDAS);  // Atualiza a média
 }
 
+// Função para exibir a lista de ajudas no DOM
 function listarAjudas() {
     lista.innerHTML = "";
     AJUDAS.forEach((ajuda, index) => {
+        // Adiciona elementos HTML para cada ajuda na lista
         lista.innerHTML += ` <div class="item">
                     <span class="name">
                     ${ajuda.nome}
@@ -57,22 +61,36 @@ function listarAjudas() {
                     <a href="javascript:removerAjuda(${index})">
                         <img src="img/trash.svg">
                     </a>
-                </div>`
+                </div>`;
     });
 }
 
+// Event listener para o botão de adicionar ajuda
 btnAdicionar.addEventListener("click", () => {
     const ajuda = {
         nome: txtPessoa.value,
         valor: txtValor.value
-    }
-    console.log(ajuda.valor);
-    AJUDAS.push(ajuda);
-    listarAjudas();
-    atualizarTotal(AJUDAS);
-    atualizarMedia(AJUDAS);
+    };
+    AJUDAS.push(ajuda);  // Adiciona a nova ajuda ao array
+    listarAjudas();  // Atualiza a lista de ajudas no DOM
+    atualizarTotal(AJUDAS);  // Atualiza o total
+    atualizarMedia(AJUDAS);  // Atualiza a média
 });
 
-
+// Inicializa a lista de ajudas no carregamento da página
 listarAjudas();
 
+// Funções para exibir um alerta indicando que o formulário foi enviado ou resetado
+function formSend() {
+    alert(`Formulário enviado!`);
+}
+
+function formReset() {
+    alert(`Formulário resetado!`);
+}
+
+// DOM é a sigla para "Document Object Model" (Modelo de Objeto de Documento).
+// Ele é uma representação em memória de documentos HTML, XML ou XHTML, que o
+// navegador utiliza para estruturar a página web. O DOM representa a estrutura
+// hierárquica do documento e permite que os scripts (como JavaScript) interajam
+// com os elementos da página, alterando dinamicamente seu conteúdo, estrutura e estilo.
